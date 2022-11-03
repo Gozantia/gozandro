80a54ce806c949029110cf716d12cacf50d364b9
gozantia <lordzantiago@gmail.com>
2022-11-03 00:29:03 -0500

    link

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://forms.gle/aT6TVDXYhxiunNcx5"
           target="_blank"
           rel="noopener noreferrer"
         >
