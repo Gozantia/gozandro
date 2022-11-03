106023a4d41360e9d0cfcfaedc00748816e64f46
gozantia <lordzantiago@gmail.com>
2022-11-03 01:00:40 -0500

    s

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Recibir el link a tu Whatsapp para escucharlo de primeras 
+          Recibir el link a tu Whatsapp para escucharlo de primera 
         </a>
       </header>
     </div>
