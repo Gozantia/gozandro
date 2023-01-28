34fd957ff5839764e4f83aa93cafeb3e37bd1d5c
gozantia <lordzantiago@gmail.com>
2023-01-28 01:05:18 -0500

    fu

--- src/App.js
+++ src/App.js
@@ -56,11 +56,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-<<<<<<< HEAD
         Instagram   </a>
-=======
-        Instacream    </a>
->>>>>>> 1fdbdc45ee908f6899da84368303c1d28124b6db
       </div>
 
     </div>
